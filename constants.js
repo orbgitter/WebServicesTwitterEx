6e3cfb98e547c6d75fc86a2d643b0878abe96d85
orbrch1 <orbrch1@gmail.com>
2019-12-22 13:47:05 +0200

    first

--- constants.js
+++ constants.js
@@ -2,6 +2,8 @@ module.exports = {
     DB_CLUSTER: 'tweetmongodbcluster',
     DB_USER: 'orbrch1',
     DB_PASS: 'einsisma2!',
-    DB_HOST: 'bkahm.mongodb.net/TweetsDatabase?retryWrites=true&w=majority'
+    DB_HOST: 'bkahm.mongodb.net/test?retryWrites=true&w=majority'
 };
 
+// mongodb+srv://orbrch1:einsisma2!@tweetmongodbcluster-bkahm.mongodb.net/TweetsDatabase?retryWrites=true&w=majority
+// mongodb+srv://orbrch1:<password>@tweetmongodbcluster-bkahm.mongodb.net/test?retryWrites=true&w=majority
